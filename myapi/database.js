6c973117017f0a3ef1676ddc7a89b1c68a2a7b5a
unknown <risto.alanen@gmail.com>
2021-02-23 13:17:33 +0200

    valmis herokuun

--- myapi/database.js
+++ myapi/database.js
@@ -1,8 +1,13 @@
 const mysql = require('mysql');
-const connection = mysql.createPool({
+/*const connection = mysql.createPool({
     host: 'localhost',
     user: 'netuser',
     password: 'netpass',
     database: 'netdb'
-});
+});*/
+
+const dotenv = require('dotenv');
+dotenv.config();
+const connection = mysql.createConnection(process.env.JAWSDB_URL);
+
 module.exports = connection;
